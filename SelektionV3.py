5cae56f2ac2458f96bc7c55c00da3f3fa22b7874
Benusheart <49153467+Benusheart@users.noreply.github.com>
2019-05-13 15:13:03 +0200

    Add files via upload

--- SelektionV3.py
+++ SelektionV3.py
@@ -63,9 +63,6 @@ html.Div(
         [Sunburst(id='sun', data=sunburst_data)],
         style={'width': '49%', 'display': 'flex', 'float': 'right'}),
 
-    dcc.Graph(
-        id='graph',
-        style={'width': '49%', 'display': 'flex', 'float': 'left'}),
     html.Div(id='output', style={'clear': 'both'}),
     html.Div(id='tabs-content-example')
 
@@ -142,76 +139,10 @@ def render_content(tab):
 
 @app.callback(Output('output', 'children'), [Input('sun', 'selectedPath')])
 def display_selected(selected_path):
+        layout={'width': '49%', 'display': 'flex', 'float': 'right'}
         return 'You have selected path: {}'.format('->'.join(selected_path or []) or 'root')
 
-@app.callback(Output('graph', 'figure'), [Input('sun', 'data'), Input('sun', 'selectedPath')])
-def display_graph(data, selected_path):
-        x = []
-        y = []
-        text = []
-        color = []
-        joined_selected = '->'.join(selected_path or [])
 
-        SELECTED_COLOR = '#03c'
-        SELECTED_CHILDREN_COLOR = '#8cf'
-        SELECTED_PARENTS_COLOR = '#f80'
-        DESELECTED_COLOR = '#ccc'
-
-        def node_color(node_path):
-            joined_node = '->'.join(node_path)
-            if joined_node == joined_selected:
-                return SELECTED_COLOR
-            if joined_node.startswith(joined_selected):
-                return SELECTED_CHILDREN_COLOR
-            if joined_selected.startswith(joined_node):
-                return SELECTED_PARENTS_COLOR
-            return DESELECTED_COLOR
-
-        def append_point(child_count, size, node, node_path):
-            x.append(child_count)
-            y.append(size)
-            text.append(node['name'])
-            color.append(node_color(node_path))
-
-        def crawl(node, node_path):
-            if 'size' in node:
-                append_point(1, node['size'], node, node_path)
-                return (1, node['size'])
-            else:
-                node_count, node_size = 1, 0
-                for child in node['children']:
-                    this_count, this_size = crawl(child, node_path + [child['name']])
-                    node_count += this_count
-                    node_size += this_size
-                append_point(node_count, node_size, node, node_path)
-                return (node_count, node_size)
-
-        crawl(data, [])
-
-        layout = {
-            'width': 500,
-            'height': 500,
-            'xaxis': {'title': 'Total Nodes', 'type': 'log'},
-            'yaxis': {'title': 'Total Size', 'type': 'log'},
-            'hovermode': 'closest'
-        }
-
-        return {
-            'data': [{
-                'x': x,
-                'y': y,
-                'text': text,
-                'textposition': 'middle right',
-                'marker': {
-                    'color': color,
-                    'size': [(v * v + 100) ** 0.5 for v in y],
-                    'opacity': 0.5
-                },
-                'mode': 'markers+text',
-                'cliponaxis': False
-            }],
-            'layout': layout
-        }
 
 
 if __name__ == '__main__':
