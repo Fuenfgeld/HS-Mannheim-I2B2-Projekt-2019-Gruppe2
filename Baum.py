8f2606ce8d0c45b63f4d1bcb99236588f9aaadac
jean-luclegend <37864942+jean-luclegend@users.noreply.github.com>
2019-05-29 15:00:38 +0200

    Update Baum.py
    Singletonpattern in Baum implementiert.
    Statt dem Konstruktor darf nur noch die statische Klassen-Methode instance() aufgerufen werden. Diese sorgt dafür, dass der Konstruktor nur einmal aufgerufen werden kann. Da Python aber keine echten privaten Attribute bzw. Methoden besitzt ist ein direkter Aufruf des Konstruktors noch möglich. Sollte ein zweitesmal der Konstruktor aufgerufen werden, wirft der Konstruktor eine Exception

--- Baum.py
+++ Baum.py
@@ -4,12 +4,27 @@ import tree_dictionary_import_export as tie
 import pickle
 class Baum:
 
+    __instance = None
+
+    @staticmethod
+    def instance(self):
+        if Baum.__instance == None:
+            Baum()
+        return Baum.__instance
+
+
     def __init__(self,name):
-        self.name=name
-        self.knotenliste_mit_baum=kl.knotenliste_gesamt_erstellen()
-        #print(self.knotenliste_mit_baum)
-        exporter=DictExporter()
-        self.dict_baum=exporter.export(self.knotenliste_mit_baum[0][0])
-        tie.treedictionary_in_pickle_exportieren(self,filename=name)
+        """virtual private konstruktor"""
+        if Baum.__instance != None:
+            raise Exception
+        else:
+            self.name=name
+            self.knotenliste_mit_baum=kl.knotenliste_gesamt_erstellen()
+            #print(self.knotenliste_mit_baum)
+            exporter=DictExporter()
+            self.dict_baum=exporter.export(self.knotenliste_mit_baum[0][0])
+            tie.treedictionary_in_pickle_exportieren(self,filename=name)
+
+
 
 
