8c6908a1345e89dce72dfc69684eee719b779206
NadjaKraemer <1724781@stud.hs-mannheim.de>
2019-06-11 13:34:28 +0200

    Layout Buttons leicht verändert

--- visualisierung/website.py
+++ visualisierung/website.py
@@ -334,7 +334,6 @@ def display_selected(selected_path):
 @app.callback(Output('search-input', 'value'), [Input('reset', 'n_clicks')])
 def reset_input(n_clicks):
     if(n_clicks is not None):
-        print("hiiii")
         return ""
 
 if __name__ == '__main__':
