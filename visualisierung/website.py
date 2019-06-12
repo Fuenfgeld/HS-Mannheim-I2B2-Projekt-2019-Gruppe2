78d0992d3ee6868452a44824d2b080f3e3207015
NadjaKraemer <1724781@stud.hs-mannheim.de>
2019-06-12 10:30:02 +0200

    Farben Callback und textposition outside bei Altersdiagramm

--- visualisierung/website.py
+++ visualisierung/website.py
@@ -129,7 +129,7 @@ app.layout = html.Div([
                              figure=go.Figure(
                                  data=[go.Pie(labels=['Male', 'Female'],
                                               values=qs.bottom().geschlecht_value_counts,
-                                              marker=dict(colors=['#5CABFF', '#4875E8']))],
+                                              marker=dict(colors=['#5CABFF','#4875E8']))],
                                  layout=go.Layout(title={
                                      'text': 'Gender',
                                      'x': 0.49,
@@ -145,7 +145,7 @@ app.layout = html.Div([
                                  data=[go.Bar(x=qs.bottom().x_achse_altersverteilung,
                                               y=qs.bottom().y_achse_altersverteilung,
                                               text=qs.bottom().y_achse_altersverteilung,
-                                              textposition='auto',
+                                              textposition='outside',
                                               marker=dict(color=['#4F5EFF', '#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF',
                                                                  '#8BCAF5'])
                                               )],
@@ -173,7 +173,7 @@ app.layout = html.Div([
                             data=[go.Bar(x=qs.bottom().x_achse_altersverteilung,
                                          y=qs.bottom().y_achse_altersverteilung,
                                          text=qs.bottom().y_achse_altersverteilung,
-                                         textposition='auto',
+                                         textposition='outside',
                                          marker=dict(
                                              color=['#4F5EFF', '#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5'])
                                          )],
@@ -368,7 +368,8 @@ def update_graphs(abfrage):
         except IndexError:
             return ('Ungültige Eingabe. Ergebnisse sind von vorheriger Abfrage'), \
                    {'data': [go.Pie(labels=['Male', 'Female'],
-                                    values=qs.peek().geschlecht_value_counts)],
+                                    values=qs.peek().geschlecht_value_counts,
+                                    marker=dict(colors=['#5CABFF','#4875E8']))],
                     'layout': go.Layout(title={
                         'text': 'Gender',
                         'x': 0.49,
@@ -378,7 +379,8 @@ def update_graphs(abfrage):
                    {'data': [go.Bar(x=qs.peek().x_achse_altersverteilung,
                                     y=qs.peek().y_achse_altersverteilung,
                                     text=qs.peek().y_achse_altersverteilung,
-                                    textposition='auto',
+                                    textposition='outside',
+                                    marker = dict(color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8', '#9998FF', '#957CEB', '#BE8CFF' ])
                                     )],
                     'layout': go.Layout(title={
                         'text': 'Age',
@@ -392,34 +394,44 @@ def update_graphs(abfrage):
                    {'data': [go.Bar(x=qs.peek().x_achse_altersverteilung,
                                     y=qs.peek().y_achse_altersverteilung,
                                     text=qs.peek().y_achse_altersverteilung,
-                                    textposition='auto',
+                                    textposition='outside',
+                                    marker=dict(
+                                        color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF',
+                                               '#8093E8', '#9998FF', '#957CEB', '#BE8CFF'])
                                     )],
                     'layout': go.Layout(title='Age', xaxis=dict(title='Age groups'),
                                         yaxis=dict(title='Number of patients'))}, \
                    {'data': [go.Pie(labels=['Male', 'Female'],
-                                    values=qs.peek().geschlecht_value_counts)],
+                                    values=qs.peek().geschlecht_value_counts,
+                                    marker=dict(colors=['#5CABFF', '#4875E8']))],
                     'layout': go.Layout(title='Gender', margin={"l": 300, "r": 300}, legend={"x": 0.9, "y": 0.7})
                     }, \
                    {'data': [go.Bar(x=qs.peek().nd_prozent_value_list,
                                     y=qs.peek().nd_diagnose_value_list,
                                     text=qs.peek().nd_prozent_value_list,
                                     textposition='outside',
-                                    orientation='h', )],
+                                    orientation='h',
+                                    marker=dict(
+                                        color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF',
+                                               '#8093E8', '#9998FF', '#957CEB', '#BE8CFF']))],
                     'layout': go.Layout(title='Secondary Diagnosis', xaxis=dict(title='Number of patients in %'),
                                         yaxis=go.layout.YAxis(automargin=True, autorange="reversed", ))
                     }, \
                    {'data': [go.Pie(labels=qs.peek().racex,
-                                    values=qs.peek().racey)],
+                                    values=qs.peek().racey,
+                                    marker=dict(colors=['#4F5EFF', '#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF']))],
                     'layout': go.Layout(title='Race', margin={"l": 300, "r": 300}, legend={"x": 0.9, "y": 0.7})
                     }, \
                    {'data': [go.Pie(labels=qs.peek().sprachex,
-                                    values=qs.peek().sprachey)],
+                                    values=qs.peek().sprachey,
+                                    marker=dict(colors=['#4F5EFF', ' #5CABFF', '#37E7FF']))],
                     'layout': go.Layout(title='Language', margin={"l": 300, "r": 300},
                                         legend={"x": 0.9, "y": 0.7})
                     }
         return ('Count: ', qs.peek().kohortengröße, ' (', qs.peek().kohortengröße_prozent, '%)'), \
                {'data': [go.Pie(labels=['Male', 'Female'],
-                                values=qs.peek().geschlecht_value_counts)],
+                                values=qs.peek().geschlecht_value_counts,
+                                marker=dict(colors=['#5CABFF', ' #4875E8']))],
                 'layout': go.Layout(title={
                     'text': 'Gender',
                     'x': 0.49,
@@ -429,7 +441,8 @@ def update_graphs(abfrage):
                {'data': [go.Bar(x=qs.peek().x_achse_altersverteilung,
                                 y=qs.peek().y_achse_altersverteilung,
                                 text=qs.peek().y_achse_altersverteilung,
-                                textposition='auto',
+                                textposition='outside',
+                                marker = dict(color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8', '#9998FF', '#957CEB', '#BE8CFF' ])
                                 )],
                 'layout': go.Layout(title={
                     'text': 'Age',
@@ -443,35 +456,43 @@ def update_graphs(abfrage):
                {'data': [go.Bar(x=qs.peek().x_achse_altersverteilung,
                                 y=qs.peek().y_achse_altersverteilung,
                                 text=qs.peek().y_achse_altersverteilung,
-                                textposition='auto',
+                                textposition='outside',
+                                marker = dict(color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8', '#9998FF', '#957CEB', '#BE8CFF' ])
                                 )],
                 'layout': go.Layout(title='Age', xaxis=dict(title='Age groups'),
                                     yaxis=dict(title='Number of patients'))}, \
                {'data': [go.Pie(labels=['Male', 'Female'],
-                                values=qs.peek().geschlecht_value_counts)],
+                                values=qs.peek().geschlecht_value_counts,
+                                marker=dict(colors=['#5CABFF', ' #4875E8']))],
                 'layout': go.Layout(title='Gender', margin={"l": 300, "r": 300}, legend={"x": 0.9, "y": 0.7})
                 }, \
                {'data': [go.Bar(x=qs.peek().nd_prozent_value_list,
                                 y=qs.peek().nd_diagnose_value_list,
                                 text=qs.peek().nd_prozent_value_list,
                                 textposition='outside',
-                                orientation='h', )],
+                                orientation='h',
+                                marker=dict(
+                                    color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8',
+                                           '#9998FF', '#957CEB', '#BE8CFF']))],
                 'layout': go.Layout(title='Secondary Diagnosis', xaxis=dict(title='Number of patients in %'),
                                     yaxis=go.layout.YAxis(automargin=True, autorange="reversed", ))
                 }, \
                {'data': [go.Pie(labels=qs.peek().racex,
-                                values=qs.peek().racey)],
+                                values=qs.peek().racey,
+                                marker=dict(colors=['#4F5EFF', '#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF']))],
                 'layout': go.Layout(title='Race', margin={"l": 300, "r": 300}, legend={"x": 0.9, "y": 0.7})
                 }, \
                {'data': [go.Pie(labels=qs.peek().sprachex,
-                                values=qs.peek().sprachey)],
+                                values=qs.peek().sprachey,
+                                marker=dict(colors=['#4F5EFF', ' #5CABFF', '#37E7FF']))],
                 'layout': go.Layout(title='Language', margin={"l": 300, "r": 300},
                                     legend={"x": 0.9, "y": 0.7})
                 }
 
     return ('Count: ', qs.bottom().kohortengröße, ' (', qs.bottom().kohortengröße_prozent, '%)'), {
         'data': [go.Pie(labels=['Male', 'Female'],
-                        values=qs.bottom().geschlecht_value_counts)],
+                        values=qs.bottom().geschlecht_value_counts,
+                        marker=dict(colors=['#5CABFF', ' #4875E8']))],
         'layout': go.Layout(title={
             'text': 'Gender',
             'x': 0.49,
@@ -481,7 +502,8 @@ def update_graphs(abfrage):
            {'data': [go.Bar(x=qs.bottom().x_achse_altersverteilung,
                             y=qs.bottom().y_achse_altersverteilung,
                             text=qs.bottom().y_achse_altersverteilung,
-                            textposition='auto',
+                            textposition='outside',
+                            marker = dict(color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8', '#9998FF', '#957CEB', '#BE8CFF' ])
                             )],
             'layout': go.Layout(title={
                 'text': 'Age',
@@ -495,28 +517,35 @@ def update_graphs(abfrage):
            {'data': [go.Bar(x=qs.bottom().x_achse_altersverteilung,
                             y=qs.bottom().y_achse_altersverteilung,
                             text=qs.bottom().y_achse_altersverteilung,
-                            textposition='auto',
+                            textposition='outside',
+                            marker = dict(color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8', '#9998FF', '#957CEB', '#BE8CFF' ])
                             )],
             'layout': go.Layout(title='Age', xaxis=dict(title='Age groups'),
                                 yaxis=dict(title='Number of patients'))}, \
            {'data': [go.Pie(labels=['Male', 'Female'],
-                            values=qs.bottom().geschlecht_value_counts)],
+                            values=qs.bottom().geschlecht_value_counts,
+                            marker=dict(colors=['#5CABFF', ' #4875E8']))],
             'layout': go.Layout(title='Gender', margin={"l": 300, "r": 300}, legend={"x": 0.9, "y": 0.7})
             }, \
            {'data': [go.Bar(x=qs.bottom().nd_prozent_value_list,
                             y=qs.bottom().nd_diagnose_value_list,
                             text=qs.bottom().nd_prozent_value_list,
                             textposition='outside',
-                            orientation='h', )],
+                            orientation='h',
+                            marker=dict(
+                                color=['#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF', '#8BCAF5', '#75AAFF', '#8093E8',
+                                       '#9998FF', '#957CEB', '#BE8CFF']))],
             'layout': go.Layout(title='Secondary Diagnosis', xaxis=dict(title='Number of patients in %'),
                                 yaxis=go.layout.YAxis(automargin=True, autorange="reversed",
                                                       ))}, \
            {'data': [go.Pie(labels=qs.bottom().racex,
-                            values=qs.bottom().racey)],
+                            values=qs.bottom().racey,
+                            marker=dict(colors=['#4F5EFF', '#4875E8', ' #5CABFF', '#48B5E8', '#37E7FF']))],
             'layout': go.Layout(title='Race', margin={"l": 300, "r": 300}, legend={"x": 0.9, "y": 0.7})
             }, \
            {'data': [go.Pie(labels=qs.bottom().sprachex,
-                            values=qs.bottom().sprachey)],
+                            values=qs.bottom().sprachey,
+                            marker=dict(colors=['#4F5EFF', ' #5CABFF', '#37E7FF']))],
             'layout': go.Layout(title='Language', margin={"l": 300, "r": 300},
                                 legend={"x": 0.9, "y": 0.7})
             }
